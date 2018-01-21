786bee0ca368ec6cd14d5ab1576436cd38a541af
Joe Kaiser <jotato@protonmail.com>
2018-01-21 15:23:33 -0800

    example multi-cube block render - #1

--- src/main/java/mod/runeology/blocks/BlockRunestone.java
+++ src/main/java/mod/runeology/blocks/BlockRunestone.java
@@ -1,9 +1,23 @@
 package mod.runeology.blocks;
 
 import mod.runeology.NameRegistry;
+import net.minecraft.block.state.IBlockState;
 
 public class BlockRunestone extends BlockBase {
 	public BlockRunestone() {
 		super(NameRegistry.BLOCKNAME_RUNESTONE);
 	}
+	
+	   /**
+     * Used to determine ambient occlusion and culling when rebuilding chunks for render
+     */
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+
+//    public boolean isFullCube(IBlockState state)
+//    {
+//        return false;
+//    }
 }
